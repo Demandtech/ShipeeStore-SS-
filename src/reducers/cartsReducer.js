5b03363cf6a02b65ba3dec2d33fe126ef77bdef9
Demand Btc <102271199+Demandtech@users.noreply.github.com>
2023-03-20 07:10:48 +0100

    add to cart from single page and products page

--- src/reducers/cartsReducer.js
+++ src/reducers/cartsReducer.js
@@ -2,29 +2,42 @@ import {
   ADD_TO_CART,
   GET_PRODUCTS,
   DELETE_CART_ITEM,
-  TOGGLE_QUANTITY,
+  SINGLE_ADD_TO_CART,
 } from '../actions'
 
 const cartsReducer = (state, action) => {
   switch (action.type) {
     case GET_PRODUCTS:
-      return { ...state, all_product: action.payload }
+      return {
+        ...state,
+        all_products: action.payload.products,
+        single_product: action.payload.singleProduct,
+      }
     case ADD_TO_CART:
-      const itemToAddId = action.payload.id
+      const itemToAddId = action.payload
       const itemExist = state.cart.some((item) => item.id === itemToAddId)
-      console.log(itemToAddId)
-
+      //console.log(itemExist)
       if (!itemExist) {
-        let cartItem = state.all_product.find((item) => item.id === itemToAddId)
-        return {
-          ...state,
-          cart: [
-            ...state.cart,
-            { ...cartItem, discountPrice: action.payload.discountPrice },
-          ],
-        }
+        let cartItem = state.all_products.find(
+          (item) => item.id === itemToAddId
+        )
+        console.log(cartItem)
+        return { ...state, cart: [...state.cart, cartItem] }
       } else {
         return { ...state }
+      }
+
+    case SINGLE_ADD_TO_CART:
+      const itemToAdd = state.single_product
+      const itemIndexToAdd = state.cart.findIndex(
+        (item) => item.id === itemToAdd.id
+      )
+      if (itemIndexToAdd !== -1) {
+        let newItems = [...state.cart]
+        newItems[itemIndexToAdd] = itemToAdd
+        return { ...state, cart: [...newItems] }
+      } else {
+        return { ...state, cart: [...state.cart, itemToAdd] }
       }
 
     case DELETE_CART_ITEM:
